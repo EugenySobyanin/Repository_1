7f486de125b141a2a3d144b5dac209f07fe901bb
Ускова Ксения Вадимовна <pc1013400360@DTDF.ru>
2024-02-02 16:47:30 +0300

    Ставки на КХЛ и ДЗ по C++

--- Яндекс/Алгоритмы/main.py
+++ Яндекс/Алгоритмы/main.py
@@ -1,37 +1,14 @@
-import inspect
+all_convs = {1 : 1,
+             2 : 2,
+             3 : 4,
+             4 : 8,
+             5 : 16,
+             6 : 32,
+             7 : 64,
+             8 : 128,
+             9 : 256,
+             10: 489}
 
+print(sum(all_convs.values()))
 
-def print_call_stack():
-    """Функция распечатки имён функций в стеке."""
-    print([frame.function for frame in inspect.stack()])
-
-
-class Matryoshka:
-
-    def __init__(self, size, item=None):
-        self.size = size
-        self.inner_item = item
-
-
-def disassemble_matryoshka(matryoshka):
-    """Функция разборки матрёшки."""
-    # Добавим распечатку стека в начале функции.
-    print_call_stack()
-    inner_item = matryoshka.inner_item
-    if inner_item is None:
-        print(f'Все матрёшки разобраны! Размер последней матрёшки: {matryoshka.size}')
-        return
-    # Если внутри что-то есть, то печатаем информационное сообщение...
-    print(f'Разобрали матрёшку размера {matryoshka.size}, разбираем дальше!')
-    disassemble_matryoshka(inner_item)
-    # Добавим распечатку стека в конце функции.
-    print_call_stack()
-
-
-if __name__ == '__main__':
-    # Добавим распечатку стека в самом начале выполнения программы.
-    print_call_stack()
-    big_matryoshka = Matryoshka('L', Matryoshka('M', Matryoshka('S')))
-    disassemble_matryoshka(big_matryoshka)
-    # Добавим распечатку стека в конце выполнения программы.
-    print_call_stack() 
+print(489 + 256 + 32)
