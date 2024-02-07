fa7e332273a74f1b197d00c7d70ce8bde9d3afa0
Ускова Ксения Вадимовна <pc1013400360@DTDF.ru>
2024-02-07 15:56:05 +0300

    Ставки на КХЛ и НХЛ

--- Яндекс/Алгоритмы/Контест/7.сортировка_по_шаблону.py
+++ Яндекс/Алгоритмы/Контест/7.сортировка_по_шаблону.py
@@ -3,12 +3,60 @@
 Урок: Сортировка вставками
 '''
 
+import sys
+
 # решение 1
-test_arr1 = [2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19]
-pattern1 = [2, 1, 4, 3, 9, 6]
+test_arr1 = [8, 4, 9, 3, 10, 3, 3,]
+pattern1 = [8]
 
 def pattern_sort(arr, pattern):
     
-    for i in range(len(pattern)):
-        for j in range(len(arr)):
+    indx_mask = 0
+    indx_sort = 0
+    
+    for pat in range(len(pattern)):
+        for ar in range(len(arr)):
+            
+            #надо проверить эту часть кода ........................
+            if arr[ar] == pattern[pat]:
+                temp = arr[ar]
+                arr[ar] = pattern[indx_mask]
+                arr[indx_mask] = temp
+                indx_mask += 1
+                
+    
+    for el in range(indx_mask + 1, len(arr)):
+        
+        current = arr[el]
+        prev = el -1
+        
+        while prev >= indx_mask - 2 and arr[prev] > current:
+            arr[prev + 1] = arr[prev]
+            prev -= 1
+            
+        arr[prev + 1] = current
+
+pattern_sort(test_arr1, pattern1)
+
+print(test_arr1)
+                
+# if __name__ == '__main__':
+    
+#     len_arr = int(sys.stdin.readline().rstrip())
+#     #arr1 = [int(sys.stdin.readline().rstrip()) for el in range(len_arr)]
+#     arr1 = sys.stdin.readline().rstrip().split()
+#     arr1 = [int(el) for el in arr1] 
+    
+#     len_mask = int(sys.stdin.readline().rstrip())
+#     #mask1 = [int(sys.stdin.readline().rstrip()) for el in range(len_mask)]
+#     mask1 = sys.stdin.readline().rstrip().split()
+#     mask1 = [int(el) for el in mask1]
+    
+#     pattern_sort(arr1, mask1)
+#     print(' '.join([str(el) for el in arr1]))
+        
+ 
+ 
+            
+            
             
