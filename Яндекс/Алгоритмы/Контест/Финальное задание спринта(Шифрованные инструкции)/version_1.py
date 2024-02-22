5ca1cb64cb3714a62fbaf9e6ee306543074c3abf
Евгений Собянин <ea.sobyanin2018@gmail.com>
2024-02-23 01:04:07 +0300

    НХЛ

--- Яндекс/Алгоритмы/Контест/Финальное задание спринта(Шифрованные инструкции)/version_1.py
+++ Яндекс/Алгоритмы/Контест/Финальное задание спринта(Шифрованные инструкции)/version_1.py
@@ -1,4 +1,4 @@
-# id посылки 107665224
+# id посылки 107934539
 import sys
 
 
@@ -6,24 +6,24 @@ def decoding(code: str) -> str:
     '''
     Возвращает расшифрованную строку - последовательность букв.
     '''
-    stack: list = [tuple(int, str)]
+    stack: list = [tuple[int, str]]
     current_num: int = 0
-    current_str: str = ''
-    
+    result: str = ''
+
     for el in code:
         if el.isdigit():
             current_num = current_num * 10 + int(el)
         elif el == '[':
-            stack.append((current_num, current_str))
+            stack.append((current_num, result))
             current_num = 0
             result = ''
         elif el == ']':
             prev_num, prev_str = stack.pop()
             result = prev_str + result * prev_num
-        else: 
+        else:
             result += el
     return result
-    
-    
+
+
 if __name__ == '__main__':
     print(decoding(sys.stdin.readline().rstrip()))
