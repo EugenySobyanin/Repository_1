eb13d19ddb31430692e39239b19e3f797d949f74
Ускова Ксения Вадимовна <pc1013400360@DTDF.ru>
2024-03-19 16:26:15 +0300

    Ставки и морской бой

--- Тор Academy/Конфигурирование Windows 10/Лид код75/task1.py
+++ Тор Academy/Конфигурирование Windows 10/Лид код75/task1.py
@@ -1,25 +1,33 @@
 
-def sumStr(a, b):
+# 1768. Merge Strings Alternately
+
+
+def mergeAlternately(word1: str, word2: str) -> str:
     result = ''
-    a2= a
-    b2 = b
+    word_1_2 = word1
+    word_2_2 = word2
     
-    for i in range(min(len(a), len(b))):
-        result += a[i]
-        a2 = a2[1:]
-        result += b[i]
-        b2 = b2[1:]
-
-    if (a2 == ''):
-        reminder = b2
-    else:
-        reminder = a2
-
+    for i in range(min(len(word1), len(word2))):
+        result += word1[i]
+        # После добавления i - ог эл.та, удаляем перый символ у скопированной строки
+        word_1_2 = word_1_2[1:]
+        result += word2[i]
+        word_2_2 = word_2_2[1:]
+    
+    reminder = word_2_2 if word_1_2 == '' else word_1_2
     return result + reminder
 
 
-test1 = 'abcd'
-test2 = 'defxerox'
-
-
-print(sumStr(test1, test2))
+if __name__ == '__main__':
+    word1 = '1234'
+    word2 = 'defxerox'
+    print(mergeAlternately(word1, word2))
+    
+    word3 = 'ab55'
+    word4 = "pq"
+    print(mergeAlternately(word3, word4))
+    
+    word5 = ''
+    word6 = 'href'
+    print(mergeAlternately(word5, word6))
+    
