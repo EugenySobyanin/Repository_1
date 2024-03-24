c5f781cbcb88f847627e277377954bc7c417b966
Евгений Собянин <ea.sobyanin2018@gmail.com>
2024-03-24 22:31:09 +0300

    Выходные

--- Python/main.py
+++ Python/main.py
@@ -1,26 +1,10 @@
-from random import randint
+import datetime as dt
 
 
-def counting_sort(arr: list, maximum: int) -> list:
-    #Создаем массив для подсчета всех значений
-    count = [0] * (maximum + 1)
-
-    for item in arr:
-        # Для каждого значения массива array увеличиваем счётчик 
-        # в соответствующей ячейке массива count.
-        # Например, увидели в array значение 2 - добавляем единицу к значению count[2].
-        count[item] += 1
-
-    sorted_arr = []
-
-    for item in range(maximum + 1):
-        # Добавляем в sorted_array уникальный элемент столько раз, 
-        # сколько он встретился в исходном массиве.
-        sorted_arr += [item] * count[item]
-
-    return sorted_arr
+dt_now = dt.datetime.now()
 
 
-test_arr = [randint(0, 30) for el in range(1, 50)]
-result = counting_sort(test_arr, 30)
-print(result)
+
+test = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+print(test[:-5:-1])
