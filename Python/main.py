992141acd0abb18bcaa73fac272f9865cc439784
Евгений Собянин <ea.sobyanin2023@gmail.com>
2024-06-08 13:33:49 +0300

    Проверка gitignore

--- Python/main.py
+++ Python/main.py
@@ -1,26 +1,7 @@
-import time
+def foo(num):
+    if num > 0:
+        return [el for el in range(num+1) if not (el % 2)]
+    return [el for el in range(num, 1)if not (el % 2)]
 
 
-def sleep_one_sec():
-    # Запоминаем время перед выполнением исходной функции.
-    start_time = time.time()
-    # Выполняем код исходной функции:
-    time.sleep(1)
-    # Вычисляем, округляем и печатаем разницу
-    # между временем старта и актуальным временем.
-    execution_time = round(time.time() - start_time, 1)
-    print(f'Время выполнения функции: {execution_time} сек.')
-    return 'Результат первой функции.'
-
-
-def sleep_two_sec():
-    start_time = time.time()
-    # Теперь выполняем код исходной функции:
-    time.sleep(2)
-    execution_time = round(time.time() - start_time, 1)
-    print(f'Время выполнения функции: {execution_time} сек.')
-    return 'Результат второй функции.'
-
-
-sleep_one_sec()
-sleep_two_sec()
+print(foo(-10))
