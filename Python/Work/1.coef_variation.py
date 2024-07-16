bf3d6539caf57e48db147f83625e1a491d804a53
Ускова Ксения Вадимовна <pc1013400360@DTDF.ru>
2024-07-16 17:53:55 +0300

    Вторник-работа.

--- Python/Work/1.coef_variation.py
+++ Python/Work/1.coef_variation.py
@@ -18,14 +18,9 @@ from math import sqrt
 # print(standard_deviation)
 # print(variation_coef)
 
-laboratory = [
-    2_300_000,
-    2_153_100,
-    2_284_860
-]
-
 
 def find_variation_coef(coasts: list):
+    """"Расчет коэф.вариации и средней цены на основании списка с коммерческими предложениями."""
     AVG_coast = round(sum(coasts) / len(coasts), 2)
     standard_deviation = 0
     for i in range(len(coasts)):
@@ -38,5 +33,15 @@ def find_variation_coef(coasts: list):
             f'Коэф. вариации= {variation_coef}\n'
             )
 
-print(find_variation_coef(laboratory))
+# Входные данные
+hranitel_ptd = sum([3_544_200.00, 3_544_200.00, 2_801_700.00, 9_839_300.00, 3_544_200.00])
+vostok = sum([3_705_176.00, 3_690_176.00, 2_947_886.00, 10_207_164.00, 3_691_176.00])
+nadezhny = sum([3_728_264.00, 3_714_264.00, 2_966_804.00, 10_260_056.00, 3_714_264.00])
+
+print(f'Хранитель-ПТБ: {hranitel_ptd:,}'.replace(',', ' '))
+print(f'Восток       : {vostok:,}'.replace(',', ' '))
+print(f'Надежный     : {nadezhny:,}\n'.replace(',', ' '))
+
+# Вызов функции для расчета
+print(find_variation_coef([hranitel_ptd, vostok, nadezhny]))
 
