7c625386deb473c1ec3253bae7493d8c67d0b12b
Евгений Собянин <ea.sobyanin2023@gmail.com>
2024-11-26 23:28:19 +0300

    Скрипт для копирования файлов на работе.

--- Python/Work/2. servs/workdir/main_2.py
+++ Python/Work/2. servs/workdir/main_2.py
@@ -1,5 +1,6 @@
 import requests
 
+from docxtpl import DocxTemplate
 from docx import Document
 from openpyxl import load_workbook
 from num2words import num2words
@@ -26,32 +27,49 @@ def get_str_numbers(data):
 # Создаем файлы и заполняем их данными
 def make_files(data):
     for key, value in data.items():
+        template_path = "workdir/example_2.docx"
         new_file_path = f"workdir/files/{key}.docx"
         year_coast = str(value[0])
         year_coast_str = value[3]
         days = str(value[2])
         period_coast_str = value[-1]
         period_coast_rub = str(int(value[1]))
-        period_coast_penny = str(value[1])[-2:]
-        correct_ruble_case_for_year = get_correct_ruble_case(year_coast)
-        correct_ruble_case_for_period = get_correct_ruble_case(period_coast_rub)
-        correct_penny_case_for_period = get_correct_penny(period_coast_penny)
+        period_coast_penny = correct_penny(value[1])[-2:]
+        correct_ruble_case_year = get_correct_ruble_case(year_coast)
+        correct_ruble_case_period = get_correct_ruble_case(period_coast_rub)
+        correct_penny_case_period = get_correct_penny(period_coast_penny)
 
-        extracted_text = extract_text_from_docx("workdir/example.docx")
-        extracted_text = extracted_text.replace("{№№}", key)
-        extracted_text = extracted_text.replace("{СУММ_Р}", period_coast_rub)
-        extracted_text = extracted_text.replace("{ПРОПИСЬ_Р}", period_coast_str)
-        extracted_text = extracted_text.replace("{РУБ_Р}", correct_ruble_case_for_period)
-        extracted_text = extracted_text.replace("{КОПЕЙКИ}", period_coast_penny)
-        extracted_text = extracted_text.replace("{КОП_Р_СКЛ}", correct_penny_case_for_period)
-        extracted_text = extracted_text.replace("{СУММ_Г}", year_coast)
-        extracted_text = extracted_text.replace("{ПРОПИСЬ_Г}", year_coast_str)
-        extracted_text = extracted_text.replace("{РУБ_Г}", correct_ruble_case_for_year)
-        extracted_text = extracted_text.replace("{КОЛВО_ДНЕЙ}", days)
+        # extracted_text = extract_text_from_docx("workdir/example.docx")
+        # extracted_text = extracted_text.replace("{№№}", key)
+        # extracted_text = extracted_text.replace("{СУММ_Р}", period_coast_rub)
+        # extracted_text = extracted_text.replace("{ПРОПИСЬ_Р}", period_coast_str)
+        # extracted_text = extracted_text.replace("{РУБ_Р}", correct_ruble_case_period)
+        # extracted_text = extracted_text.replace("{КОПЕЙКИ}", period_coast_penny)
+        # extracted_text = extracted_text.replace("{КОП_Р_СКЛ}", correct_penny_case_period)
+        # extracted_text = extracted_text.replace("{СУММ_Г}", year_coast)
+        # extracted_text = extracted_text.replace("{ПРОПИСЬ_Г}", year_coast_str)
+        # extracted_text = extracted_text.replace("{РУБ_Г}", correct_ruble_case_year)
+        # extracted_text = extracted_text.replace("{КОЛВО_ДНЕЙ}", days)
 
-        new_doc = Document()
-        new_doc.add_paragraph(extracted_text)
-        new_doc.save(new_file_path)
+        # new_doc = Document()
+        # new_doc.add_paragraph(extracted_text)
+        # new_doc.save(new_file_path)
+
+        doc = DocxTemplate(template_path)
+        context = {
+            'key': key,
+            'year_coast': year_coast,
+            'year_coast_str': year_coast_str,
+            'days': days,
+            'period_coast_str': period_coast_str,
+            'period_coast_rub': period_coast_rub,
+            'period_coast_penny': period_coast_penny,
+            'correct_ruble_case_year': correct_ruble_case_year,
+            'correct_ruble_case_period': correct_ruble_case_period,
+            'correct_penny_case_period': correct_penny_case_period,
+        }
+        doc.render(context)
+        doc.save(new_file_path)
 
 # Подбирает склонение рубля для числа
 def get_correct_ruble_case(number: str) -> str:
@@ -84,6 +102,12 @@ def extract_text_from_docx(file_path):
     for paragraph in doc.paragraphs:
         text.append(paragraph.text)
     return '\n'.join(text)
+
+
+def correct_penny(number: float) -> float:
+    if str(number)[-2] == ".":
+        return str(number) + "0"
+    return str(number)
 
 
 # Собираем данные из файла exal
