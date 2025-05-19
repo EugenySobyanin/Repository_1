9f7a19e2b0d55afcb41693eeaf2399f8a837c2a8
ea.sobyanin2018@gmail.com <PC1013487026@DTDF.ru>
2025-05-19 10:58:38 +0300

    Уборка мусора всякого.

--- Python/DJANGO/1. Kino_gallery/kino_gallery/films/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from films.models import Genre, Type, Country, Director
-
-
-admin.site.register(Genre)
-admin.site.register(Type)
-admin.site.register(Country)
-admin.site.register(Director)
