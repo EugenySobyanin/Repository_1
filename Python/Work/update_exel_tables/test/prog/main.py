3f7dd61de4c125a0c349afa8e97f36ffea085450
ea.sobyanin2018@gmail.com <PC1013487026@DTDF.ru>
2025-11-25 13:01:35 +0300

    Доработка программы.

--- Python/Work/update_exel_tables/test/prog/main.py
+++ Python/Work/update_exel_tables/test/prog/main.py
@@ -13,6 +13,16 @@ from constants import (FINANCIAL_COLUMNS_A,
 logging.basicConfig(level=logging.INFO)
 
 
+def concatenate_kbk_str(data: dict) -> str:
+    """Формирует строку с КБК для информации об обновлении."""
+    kbk_parts = [
+        str(data[kbk_field]) 
+        for kbk_field in KBK_COLUMNS 
+        if kbk_field in data and data[kbk_field] is not None
+    ]
+    return f'Изменен КБК: {".".join(kbk_parts)}'
+
+
 def update_table_a_with_b(table_a_path, table_b_path):
     """
     Обновляет таблицу A данными из таблицы B с сохранением форматирования
@@ -25,6 +35,10 @@ def update_table_a_with_b(table_a_path, table_b_path):
     except Exception as e:
         logging.critical(f'Произошла ошибка при чтении файлов: {e}')
         return
+    
+    # 2. Заменяем NaN на None для корректной обработки
+    df_a = df_a.where(pd.notna(df_a), None)
+    df_b = df_b.where(pd.notna(df_b), None)
     
     # 2. Проверяем наиличие всех колонок в обеих таблицах
     # logging.info("Колонки в таблице A:", df_a.columns.tolist())
@@ -39,7 +53,7 @@ def update_table_a_with_b(table_a_path, table_b_path):
     if missing_in_b:
         logging.critical(f"В таблице B отсутствуют колонки: {missing_in_b}")
         return
-            
+                
     # 3. Создание словаря для сопоставления колонок
     column_mapping = dict(zip(FINANCIAL_COLUMNS_B, FINANCIAL_COLUMNS_A))
     
@@ -83,6 +97,7 @@ def update_table_a_with_b(table_a_path, table_b_path):
         # Получаем индекс колонки, которую будем проверять
         col_idx = column_indices[col_a]
         changes_count = 0
+        changes_kbk = []
         
         for row_idx, (_, row_data) in enumerate(merged.iterrows(), header_row + 1):
             original_val = sheet.cell(row=row_idx, column=col_idx).value
@@ -90,14 +105,16 @@ def update_table_a_with_b(table_a_path, table_b_path):
             
             # Проверяем, нужно ли обновлять значение
             if pd.notna(new_val) and original_val != new_val:
-                # Формируем строку с информацией о КБК
-                # print(f'{row_data['Вед']}.{row_data['Подр']}.{row_data['ЦСт']}.{row_data['Pacx']}.{row_data['КОСГУ']}.{}')
-                    
                 sheet.cell(row=row_idx, column=col_idx).value = new_val
+                changes_kbk.append(concatenate_kbk_str(row_data))
                 changes_count += 1
+                
         
         if changes_count > 0:
             print(f"Обновлено {changes_count} значений в столбце {col_a}")
+            for kbk in changes_kbk:
+                print(kbk)
+            changes_kbk = []
             total_changes += changes_count
     
     # 7. Сохраняем файл с исходным форматированием
